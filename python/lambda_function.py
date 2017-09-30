3b19a03c7eb60643ade8fc36769d0df0e9afa753
Chris <christian.loritz@live.de>
2017-09-30 15:45:00 +0200

    add shell script

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -1,20 +1,69 @@
+RUNTIME_LIST = []
+
 def lambda_handler(event, context):
     import sys
     import json
+    import os
+    import datetime
 
-    print("makefile test version2")
+    # global RUNTIME_LIST
 
-    print(sys.version)
+    # system_infos = {}
+
+    # print("makefile test version2")
+
+    # print(sys.version)
     
-    for e in event:
-        print(e)
+    # iuid = os.getuid()
+
+    # print("Event Data")
+    # system_infos["event_data"] = event
+    # # for e in event:
+    # #     print(e)
+
+    # print("UID: {}".format(iuid))
+
+    # system_infos["environ"] = []
+    # print("Environment variables")
+    # for ev in os.environ:
+    #     print("Name: '{evn}', Value: '{evv}'".format(evn=ev, evv=os.environ[ev]))
+    #     system_infos["environ"].append("Name: '{evn}', Value: '{evv}'".format(evn=ev, evv=os.environ[ev]))
         
-    items = get_files_recursiv("/")
-    
-    # for i in items:
+    # # system_infos["files"] = []
+    # # items = get_files_recursiv("/")
+    # # for i in items:
+    # #     system_infos["files"].append(i)
+
+    # system_infos["pwd"] = []
+    # with open("/etc/passwd", "r") as passwd_file:
+    #     for l in passwd_file.readlines():
+    #         system_infos["pwd"].append(l)
+
+    # runtime_string = "UID: '{uid}'; Datetime: '{dt}'".format(uid=iuid, dt=str(datetime.datetime.now()))
+
+    # RUNTIME_LIST.append(runtime_string)
+
+    # for i in RUNTIME_LIST:
     #     print(i)
 
-    return json.dumps(items)
+    #with open("/etc")    
+
+    #return json.dumps(system_infos, indent=4, separators=(',', ': '))
+
+    import urllib.request
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/Miserlou/Mackenzie/master/README.md", "/tmp/test.txt")
+
+    import glob
+    items = glob.glob("/tmp/*")
+
+    for i in items:
+        print(i)
+
+    with open("/tmp/test.txt", "r") as f:
+        for c in f.readlines():
+            print(c)
+
+    return "hey"
 
 def get_files_recursiv(folder_path):
     import glob
@@ -30,10 +79,9 @@ def get_files_recursiv(folder_path):
     items = glob.glob("{}*".format(absolute_path))
 
     for item in items:
-        if item in ["/usr", "/var", "/proc"]:
+        if item.startswith(("/usr", "/var", "/proc", "/dev")):
             continue
 
-        print("item: {}".format(item))
         if os.path.isdir(item):
             f = get_files_recursiv(item)
             files.append(f)
